5df43ba4e15c36dc78610ae8d322373fa5cf9b2f
AnastaciaZ <zaikina.anastacia@gmail.com>
2021-03-06 11:04:49 +0200

    phonebook+store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,6 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import {
-    //persistStore,
-    //persistReducer,
   FLUSH,
   REHYDRATE,
   PAUSE,
@@ -10,7 +8,6 @@ import {
   PURGE,
   REGISTER
 } from 'redux-persist';
-//import storage from 'redux-persist/lib/storage';
 import contactsReducer from './contacts/reducer';
 
 const middleware = [
@@ -22,25 +19,17 @@ const middleware = [
     logger,
 ];
 
-/*const contactsPersistConfig = {
-    key: 'contacts',
-    storage,
-    blacklist:['filter'],
-};*/
-
 const store = configureStore({
     reducer: {
-    //contacts: persistReducer(contactsPersistConfig, contactsReducer),
         contacts: contactsReducer,
 },
     middleware,
     devTools: process.env.NODE_ENV === 'development',
 });
 
-//const persistor = persistStore(store);
+
 
 const contactsStore = {
-    //persistor,
     store
 };
 
